c22b4ac12bb9202d0a5547139bba32bd9e59b0c9
𝙴𝚖𝚙𝚒𝚛𝚎 𝚃𝚎𝚌𝚑 <151479661+efeurhobobullish@users.noreply.github.com>
2025-02-22 08:00:30 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,8 +1,24 @@
 // USE ENVIRONMENT VARIABLES FOR EMAIL AND PASSWORD INSTEAD.
 module.exports = {
-  PREFIX: process.env.PREFIX || "IRON-M4N×",
-  EMAIL: process.env.EMAIL || "ironman@onlyfans.com",
-  PASS: process.env.PASS || "Kimi da yo kimi nandayo..",
-  MESSAGE: process.env.MESSAGE || "Thank you for using our bot dont share your session id blah blah .etc",
-  IMAGE: process.env.IMAGE || "https://cdn.ironman.my.id/i/2iceb4.jpeg",
-}
+  PREFIX: process.env.PREFIX || "𝙴𝙼𝙿𝙸𝚁𝙴-𝙼𝙳",
+  EMAIL: process.env.EMAIL || "empiretech@duck.com",
+  PASS: process.env.PASS || "Jehovah@1B",
+  MESSAGE: process.env.MESSAGE || `
+╭────「 𝐂𝐎𝐍𝐍𝐄𝐂𝐓𝐄𝐃 」────◆  
+│ ∘ ʀᴇᴘᴏ:  
+│ ∘ tinyurl.com/Empire-Tech  
+│──────────────────────
+│ ∘ Gʀᴏᴜᴘ:  
+│ ∘ tinyurl.com/EMPIRE-MD-GROUP  
+│──────────────────────
+│ ∘ CHANNEL:  
+│ ∘ tinyurl.com/EMPIRE-MD-CHANNEL  
+│──────────────────────
+│ ∘ Yᴏᴜᴛᴜʙᴇ:  
+│ ∘ youtube.com/only_one_empire  
+│──────────────────────
+│ ∘ 𝙴𝙼𝙿𝙸𝚁𝙴-𝙼𝙳 𝙿𝚘𝚠𝚎𝚛𝚎𝚍 𝚋𝚢 𝙴𝚖𝚙𝚒𝚛𝚎 𝚃𝚎𝚌𝚑  
+╰──────────────────────
+`,
+  IMAGE: process.env.IMAGE || "https://files.catbox.moe/gvg6ww.jpg",
+};
